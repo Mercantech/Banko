444cbfc92c59874f86d5cbe9f3244a4fc6166f27
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-11-24 07:36:24 +0100

    Fixed SLN

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -5,31 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            string gameState = "1Row";
         }
+    }
 
-        class Plate
-        {
-            string id;
-            List<int> FirstRow;
-            List<int> SecondRow;
-            List<int> FullPlate;
-
-            public void CheckOneRow(){
-                // Return if there is Banko on 1 Row
-            }
-            public void CheckTwoRow()
-            {
-            }
-
-            public void CheckFullPlate()
-            {
-
-            }
-        }
-        List<Plate> BankoPlader = new List<Plate>();
-
-        foreach (plate)
-            if (gamestate = "1row")
 }
 
